be8a722e9236ee637f2eea7d505a17179cba3040
Ibraheem Moosa <ibraheemmoosa1347@gmail.com>
2018-10-18 08:45:51 +0600

    01 minified

--- 01.hs
+++ 01.hs
@@ -1,3 +1,5 @@
-myLast [] = error "List is empty"
-myLast (x:[]) = x
-myLast (x:xs) = myLast xs
+--myLast [] = error "List is empty"
+--myLast (x:[]) = x
+--myLast (x:xs) = myLast xs
+
+myLast = foldl1' (flip const)
